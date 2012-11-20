aa175ec1bc34c5b074ee21e710c2512108361277
r00dY <a.r.dabrowski@gmail.com>
2012-11-20 18:49:50 +0100

    Dodano pokazywanie wiecej newsow

---
+++ js/newsroom_show_more.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+	
+	$('.news-grid .news-more').click(function() {
+		$('.news-grid .news-item:hidden:lt(3)').fadeIn(200);
+		if ( $('.news-grid .news-item:hidden').length == 0 ) {
+			$('.news-grid .news-more').fadeOut(200);
+		}
+		return false;
+	});
+	
+	
+});
